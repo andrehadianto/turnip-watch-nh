ee5efb8d72e5ab61ad60001cb3b68185e276f6ff
Andre Hadianto <andre_hadianto@mymial.sutd.edu.sg>
2020-04-02 00:48:32 +0800

    Updates

---
+++ precache-manifest.7a43912b363743020603185049654874.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4680cc4116c35fb69967f1f357f46334",
+    "url": "/turnip-watch-nh/index.html"
+  },
+  {
+    "revision": "ebec8e6974c0849d3ee0",
+    "url": "/turnip-watch-nh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea416e454a94144829fd",
+    "url": "/turnip-watch-nh/static/js/2.805977b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turnip-watch-nh/static/js/2.805977b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebec8e6974c0849d3ee0",
+    "url": "/turnip-watch-nh/static/js/main.7b7a7b38.chunk.js"
+  },
+  {
+    "revision": "130f282f9bec9209554a",
+    "url": "/turnip-watch-nh/static/js/runtime-main.88c843a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/turnip-watch-nh/static/media/logo.5d5d9eef.svg"
+  }
+]);
