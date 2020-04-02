3c1db3fdd7a942264191b6d6a846f85fc0404151
Andre Hadianto <andre_hadianto@mymial.sutd.edu.sg>
2020-04-02 22:52:40 +0800

    fix logic for buy price form

--- src/components/TurnipGraphChart/index.js
+++ src/components/TurnipGraphChart/index.js
@@ -35,7 +35,7 @@ export const TurnipGraphChart = () => {
         margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
       >
         <Line
-          type="stepAfter"
+          type="linear"
           dataKey="price"
           stroke="#ff4757"
           strokeWidth={1}
